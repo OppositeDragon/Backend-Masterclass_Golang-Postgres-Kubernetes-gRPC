9db1edb40938f6b45ce62aad04419d2f7b442975
OppositeDragon <stee-ven_94@outlook.com>
2023-11-20 18:00:32 -0500

    To avoid deadlock, the queries order should follow the order of the ids.

--- simple_bank/db/sqlc/store.go
+++ simple_bank/db/sqlc/store.go
@@ -82,23 +82,37 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 			return err
 		}
 		//TODO: update accounts' balance
-
-		result.FromAccount, err = q.AddAmountAccount(ctx, AddAmountAccountParams{
-			ID:     arg.FromAccountID,
-			Amount: -arg.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		result.ToAccount, err = q.AddAmountAccount(ctx, AddAmountAccountParams{
-			ID:     arg.ToAccountID,
-			Amount: arg.Amount,
-		})
-		if err != nil {
-			return err
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, _ = AddAmount(ctx, q, arg.FromAccountID, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, _ = AddAmount(ctx, q, arg.ToAccountID, arg.FromAccountID, -arg.Amount)
 		}
 		return nil
 	})
 	return result, err
+}
+
+func AddAmount(ctx context.Context,
+	q *Queries,
+	accountId1 int64,
+	accountId2 int64,
+	amount int64,
+) (
+	account1 Account,
+	account2 Account,
+	err error,
+) {
+	account1, err = q.AddAmountAccount(ctx, AddAmountAccountParams{
+		ID:     accountId1,
+		Amount: -amount,
+	})
+	if err != nil {
+		return
+	}
+	account2, err = q.AddAmountAccount(ctx, AddAmountAccountParams{
+		ID:     accountId2,
+		Amount: amount,
+	})
+	return
+
 }
