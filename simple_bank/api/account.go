97353d04e817ca983475364e939ee829be542354
OppositeDragon <stee-ven_94@outlook.com>
2023-11-21 21:26:08 -0500

    gin gets adde to project server and account endpoints are created. sqlc will return empty arrays instead of null.

---
+++ simple_bank/api/account.go
@@ -0,0 +1,125 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+	db "simple_bank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required,min=1"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR CAD GBP YEN AUD"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Balance:  0,
+		Currency: req.Currency,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountsRequest struct {
+	Offset *int32 `form:"offset" binding:"required,min=0"`
+	Limit  int32  `form:"limit" binding:"required,min=5,max=100"`
+}
+
+func (server *Server) getAccounts(ctx *gin.Context) {
+	var req getAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := db.GetAccountsParams{
+		Limit:  req.Limit,
+		Offset: *req.Offset * req.Limit,
+	}
+	accounts, err := server.store.GetAccounts(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, accounts)
+}
+
+type updateAccountRequest struct { 
+	Amount int64  `json:"amount" binding:"required,gte=0"`
+}
+
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var req1 getAccountRequest
+	var req2 updateAccountRequest
+	if err := ctx.ShouldBindUri(&req1); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	if err := ctx.ShouldBindJSON(&req2); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := db.AddAmountAccountParams{
+		ID:     req1.ID,
+		Amount: req2.Amount,
+	}
+	account, err := server.store.AddAmountAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+
+}
+
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req  getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, "Account deleted successfully")
+}
